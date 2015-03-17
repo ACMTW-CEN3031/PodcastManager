99aec37304b2fbba34fe5c1962317a1c3f7fe9ec
Wyatt Lindquist <git.wquist@gmail.com>
2015-03-17 14:40:40 -0400

    Test deck is deleted after creation.

--- public/modules/decks/tests/protractor.spec.test.js
+++ public/modules/decks/tests/protractor.spec.test.js
@@ -1,4 +1,15 @@
-//
+var getDeckPanel = function()
+{
+	var panels = element.all(by.repeater('deck in decks'));
+	for (var i = 0; i != panels.count(); ++i)
+	{
+		var panel = panels.get(i);
+		if (panel.element(by.binding('deck.name')).getText() == '*Protractor Deck')
+			return panel;
+	}
+
+	return false;
+};
 
 describe('Deck management', function()
 {
@@ -6,11 +17,19 @@ describe('Deck management', function()
 	{
 		browser.get('#!/decks/create');
 		
-		element(by.model('name')).sendKeys('Protractor Deck');
+		element(by.model('name')).sendKeys('*Protractor Deck');
 		element(by.model('description')).sendKeys('autogen');
 
 		element(by.id('create_submit')).click();
 
-		expect(element(by.binding('deck.name')).getText()).toEqual('Protractor Deck');
+		expect(element(by.binding('deck.name')).getText()).toEqual('*Protractor Deck');
+	});
+
+	it('should delete the deck', function()
+	{
+		browser.get('#!/decks');
+
+		element.all(by.repeater('deck in decks')).get(0).element(by.id('delete')).click();
+		expect(element.all(by.repeater('deck in decks')).get(0)).not.toBe('*Protractor Deck');
 	});
 });
