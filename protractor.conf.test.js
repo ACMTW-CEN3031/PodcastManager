d3a28a5336cdc42cb93ef3ef6261620e9c862c5e
mickeyjk <micohen13@gmail.com>
2015-03-20 19:10:23 -0400

    re-added other tests to conf file

--- protractor.conf.test.js
+++ protractor.conf.test.js
@@ -3,9 +3,9 @@
 exports.config =
 {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['public/modules/users/tests/authentication.signup.spec.test.js'],
+	specs: ['public/modules/users/tests/authentication.signup.spec.test.js', 'public/modules/users/tests/spec.signin.test.js','public/modules/decks/tests/protractor.spec.test.js'],
 
 	baseUrl: 'http://localhost:3000/'
 }
 
-//, 'public/modules/users/tests/spec.signin.test.js','public/modules/decks/tests/protractor.spec.test.js'
+//
