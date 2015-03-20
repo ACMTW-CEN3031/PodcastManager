c922e68102c0f37b4e58cd23842a412037d14dd8
Wyatt Lindquist <git.wquist@gmail.com>
2015-03-20 13:07:37 -0400

    Added ability to upload a profile image.

--- public/modules/users/controllers/settings.client.controller.js
+++ public/modules/users/controllers/settings.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-	function($scope, $http, $location, Users, Authentication) {
+angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', '$upload', 'Users', 'Authentication',
+	function($scope, $http, $location, $upload, Users, Authentication) {
 		$scope.user = Authentication.user;
 
 		// If user is not signed in then redirect back home
@@ -53,6 +53,56 @@ angular.module('users').controller('SettingsController', ['$scope', '$http', '$l
 			} else {
 				$scope.submitted = true;
 			}
+		};
+
+		$scope.uploadImage = function(file)
+		{
+			var user = $scope.user;
+
+			var fileReader = new FileReader();
+			fileReader.readAsArrayBuffer(file);
+			fileReader.onload = function(e)
+			{
+				$scope.uploading = true;
+
+				$upload.http({
+					url: 'https://api.imgur.com/3/image',
+					headers:
+					{
+            			Authorization: 'Client-ID ' + 'd68cf8484744ab5'
+       				},
+       				data: e.target.result
+				})
+				.success(function(result)
+				{
+					$scope.uploading = false;
+					$scope.uploadProgress = 0;
+
+					var url = result.data.link;
+					var split = url.lastIndexOf('/');
+
+					var name = url.substring(split);
+					user.thumbnail = name;
+
+					$scope.updateUserProfile(true);
+				})
+				.error(function(err)
+				{
+					$scope.uploading = false;
+					$scope.uploadProgress = 0;
+					
+					if (err.status === 400)
+						$scope.error = 'Invalid file!';
+					else
+						$scope.error = 'Could not communicate with imgur!';
+				})
+				.progress(function(evt)
+				{
+					// Decrease the percentage since 99% looks done when it is not.
+					var percent = parseInt(100.0 * evt.loaded / evt.total);
+					$scope.uploadProgress = Math.max(percent - 5, 0);
+				});
+			};
 		};
 
 		// Change user password
