716f4250c39447d262efc3a69ff1a219b78ebe6f
mickeyjk <micohen13@gmail.com>
2015-03-20 15:55:56 -0400

    Moved protractor conf file to be easier to use

--- protractor.conf.test.js
+++ protractor.conf.test.js
@@ -3,7 +3,7 @@
 exports.config =
 {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['protractor.spec.test.js'],
+	specs: ['public/modules/decks/tests/protractor.spec.test.js'],
 
 	baseUrl: 'http://localhost:3000/'
 }
