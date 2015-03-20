22adb772e8dc7b3a4a1cc9542d25e34f5489334a
Corey Goldenberg <corey_goldenberg@aol.com>
2015-03-20 16:17:56 -0400

    Protractor Test-Signin
    Tests everything related to signin and signet

---
+++ public/modules/users/tests/spec.signin.test.js
@@ -0,0 +1,52 @@
+//Login test
+describe('Authentication capabilities', function() {
+  var loginURL;
+  var email = element(by.name('username'));
+  var password = element(by.name('password'));
+  var loginButton = element(by.id('signinbutton'));
+  var error = element(by.binding('error'));
+
+  it('should redirect to the login page if trying to load protected page while not authenticated', function() {
+    browser.get('/#!/signin');
+    loginURL = browser.getCurrentUrl();
+
+    browser.get('/#!/signin');
+    expect(browser.getCurrentUrl()).toEqual(loginURL);
+  });
+
+  it('should warn on missing/malformed credentials', function() {
+    email.clear();
+    password.clear();
+
+    password.sendKeys('testtest');
+    loginButton.click();
+    expect(error.getText()).toMatch('Missing credentials');
+
+    email.sendKeys('wrongtest');
+    loginButton.click();
+    expect(error.getText()).toMatch('Unknown user');
+
+    email.sendKeys('test');
+    password.clear();
+    loginButton.click();
+    expect(error.getText()).toMatch('Missing credentials');
+  });
+
+  it('should accept a valid email address and password', function() {
+    email.clear();
+    password.clear();
+
+    email.sendKeys('test');
+    password.sendKeys('testtest');
+    loginButton.click();
+    expect(browser.getCurrentUrl()).not.toEqual(loginURL);
+  });
+
+  it('should return to the login page after logout', function() {
+    var menuDropdown = element(by.binding('authentication.user.displayName'));
+    menuDropdown.click();
+    var logoutButton = element(by.id('signoutbutton'));
+    logoutButton.click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#!/');
+  });
+});
