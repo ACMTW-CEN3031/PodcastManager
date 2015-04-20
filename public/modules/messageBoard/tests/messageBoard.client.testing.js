cc7df6a8513f38b015c2535307958f0b3870511d
Wyatt Lindquist <git.wquist@gmail.com>
2015-04-19 23:58:07 -0400

    Lots more message board karma tests.

--- public/modules/messageBoard/tests/messageBoard.client.testing.js
+++ public/modules/messageBoard/tests/messageBoard.client.testing.js
@@ -4,12 +4,51 @@ describe('messageController', function()
 {
 	beforeEach(module(ApplicationConfiguration.applicationModuleName));
 
-	var scope, $stateParams;
-	beforeEach(inject(function($rootScope, _$stateParams_)
+	var scope, $stateParams, $httpBackend;
+	beforeEach(inject(function($controller, $rootScope, _$stateParams_, _$httpBackend_)
 	{
 		scope = $rootScope.$new();
 		$stateParams = _$stateParams_;
+		$httpBackend = _$httpBackend_;
 	}));
+
+	it('Creates a post', function(messageBoardService)
+	{
+		var post = new messageBoardService({ title: 'Test', content: 'testContent', userName: 'testUser'});
+		var postResponse = new messageBoardService({ _id: '525a8422f6d0f87f0e407a33', title: 'Test', content: 'testContent', userName: 'testUser'});
+
+		$httpBackend.expectPOST('posts', post).respond(postResponse);
+	});
+
+	it('Updates a post', function(messageBoardService)
+	{
+		var post = new messageBoardService({ _id: '525a8422f6d0f87f0e407a33', title: 'Test', content: 'testContent', userName: 'testUser'});
+		scope.post = post;
+
+		$httpBackend.expectPUT('/articles/' + post._id);
+	});
+
+	it('Removes a post', function(messageBoardService)
+	{
+		var post = new messageBoardService({ _id: '525a8422f6d0f87f0e407a33', title: 'Test', content: 'testContent', userName: 'testUser'});
+		scope.posts = [post];
+
+		$httpBackend.expectDELETE('/articles/' + post._id);
+
+		scope.deletePost(post);
+		expect(scope.posts.length).toBe(0);
+	});
+
+	it('Adds a comment', function(messageBoardService)
+	{
+		var post = new messageBoardService({ _id: '525a8422f6d0f87f0e407a33', title: 'Test', content: 'testContent', userName: 'testUser'});
+
+		scope.commentText = 'test comment';
+		scope.addComment(post);
+
+		expect(post.comments.length).toBe(1);
+		expect(post.comments[0]).toEqual('test comment');
+	});
 
 	it('Finds multiple posts in a query', function(messageBoardService)
 	{
