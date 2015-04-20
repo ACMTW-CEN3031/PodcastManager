829fee30a12a0858a13cef261bc6383b8809aa2c
Wyatt Lindquist <git.wquist@gmail.com>
2015-04-20 00:21:47 -0400

    Fixed login issue with route tests.

--- app/tests/decks.server.routes.test.js
+++ app/tests/decks.server.routes.test.js
@@ -4,31 +4,58 @@ var should = require('should'),
 	request = require('supertest'),
 	app = require('../../server'),
 	mongoose = require('mongoose'),
+	User = mongoose.model('User'),
 	Deck = mongoose.model('Deck'),
 	agent = request.agent(app);
 
-var deck1, deck2;
+var credentials, user, deck1, deck2;
 describe('Deck CRUD tests', function()
 {
 	beforeEach(function(done)
 	{
-		deck1 = new Deck({ name: 'Test Deck', images: ['init.jpg'] });
-		deck2 = new Deck({ name: 'Test Deck', images: ['test.jpg'] });
+		credentials = {
+			username: 'username',
+			password: 'password'
+		};
 
-		done();
+		user = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			displayName: 'Full Name',
+			email: 'test@test.com',
+			username: credentials.username,
+			password: credentials.password,
+			provider: 'local'
+		});
+
+		user.save(function() {
+			deck1 = new Deck({ name: 'Test Deck', images: ['init.jpg'] });
+			deck2 = new Deck({ name: 'Test Deck', images: ['test.jpg'] });
+
+			done();
+		});
 	});
 
 	it('should not be able to save a deck with no name', function(done)
 	{
 		deck1.name = '';
 
-		agent.post('/decks')
-			.send(deck1)
-			.expect(400)
+		agent.post('/auth/signin')
+			.send(credentials)
+			.expect(200)
 			.end(function(err, res)
 			{
-				res.body.message.should.match('Deck must have a name');
-				done(err);
+				if (err)
+					done(err);
+
+				agent.post('/decks')
+					.send(deck1)
+					.expect(400)
+					.end(function(err, res)
+					{
+						res.body.message.should.match('Deck must have a name');
+						done(err);
+					});
 			});
 	});
 
@@ -36,19 +63,30 @@ describe('Deck CRUD tests', function()
 	{
 		deck1.save();
 
-		agent.post('/decks')
-			.send(deck2)
-			.expect(400)
+		agent.post('/auth/signin')
+			.send(credentials)
+			.expect(200)
 			.end(function(err, res)
 			{
-				res.body.message.should.match('Name already exists');
-				done(err);
+				if (err)
+					done(err);
+
+				agent.post('/decks')
+					.send(deck2)
+					.expect(400)
+					.end(function(err, res)
+					{
+						res.body.message.should.match('Name already exists');
+						done(err);
+					});
 			});
 	});
 
 	afterEach(function(done)
 	{
-		Deck.remove().exec();
-		done();
+		User.remove().exec(function()
+		{
+			Deck.remove().exec(done);
+		});
 	});
 });
