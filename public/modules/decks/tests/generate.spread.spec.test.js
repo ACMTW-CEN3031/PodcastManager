23df62c959a817e07d961ddbcfdf5ee14d328ebd
Corey Goldenberg <corey_goldenberg@aol.com>
2015-04-20 05:03:44 -0400

    Generate spread test now passes

--- public/modules/decks/tests/generate.spread.spec.test.js
+++ public/modules/decks/tests/generate.spread.spec.test.js
@@ -1,40 +1,40 @@
 describe('Spread Tests', function()
 {
-	var decks = element.all(by.repeater('deck in decks'));
 	var inSpread = element.all(by.repeater('image in cardsInSpread'));
+	var toggleOn = element(by.id('toggleOn'));
+	var inputNum = element(by.model('deck.cardCount'));
+	var refresh = element(by.id('refresh'));
 
 	beforeEach(function(){
 		browser.get('#!/generate');
 	});
 
-	it('should add all decks to the spread', function()
+	it('should add a deck to the spread', function()
 	{
-		decks.each(function(deck, index){
-			var toggleOn = deck.element(by.id('toggleOn'));
-
+			
 			toggleOn.click();
-		});
 
-		expect(decks.count()).toEqual(inSpread.count());
+			inputNum.sendKeys(1);
+
+			refresh.click();
+		
+
+			expect(inSpread.count()).toEqual(1);
 	});
 
-	it('should add then remove all decks from the spread', function(){
-		decks.each(function(deck, index){
-			var toggleOn = deck.element(by.id('toggleOn'));
+	it('should add then remove a deck from the spread', function(){
 
 			toggleOn.click();
+			inputNum.sendKeys(1);
+			refresh.click();
 
 
-		});
-		expect(inSpread.count()).toEqual(decks.count());
+		expect(inSpread.count()).toEqual(1);
 
-		decks.each(function(deck, index){
-			var toggleOff = deck.element(by.id('toggleOff'));
+			var toggleOff = element(by.id('toggleOff'));
 
 			toggleOff.click();
 
-			
-		});
 		browser.waitForAngular();
 		expect(inSpread.count()).toEqual(0);
 
