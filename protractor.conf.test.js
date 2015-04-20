85c193e2722a5aefaee57b24be623c6301d3c953
Corey Goldenberg <corey_goldenberg@aol.com>
2015-04-20 05:01:56 -0400

    Added users test to protractor.conf file

--- protractor.conf.test.js
+++ protractor.conf.test.js
@@ -6,9 +6,10 @@ exports.config =
 	specs: ['public/modules/users/tests/authentication.signup.spec.test.js',
 	 'public/modules/users/tests/signin.spec.test.js',
 	 'public/modules/users/tests/editprofile.spec.test.js',
+	 'public/modules/users/tests/users.spec.test.js',
 	 'public/modules/decks/tests/generate.spread.spec.test.js',
 	 'public/modules/decks/tests/protractor.spec.test.js'],
-	
+
 	baseUrl: 'http://localhost:3000/'
 }
 
